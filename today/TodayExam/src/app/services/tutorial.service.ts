d2e96225a6edc5ee14587d2f228d65f4a0a4668a
Rachana Priyadarshni Samal <CZ0250@cztech.com>
2023-10-10 17:19:50 +0530

    first commit

--- today/TodayExam/src/app/services/tutorial.service.ts
+++ today/TodayExam/src/app/services/tutorial.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://localhost:8080/api';
 
 @Injectable({
   providedIn: 'root'
